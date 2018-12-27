cf5498e1101b435e7d454ac6d6e044ea4184b30a
Nolan Kornelsen <perpetualcolor@gmail.com>
2018-12-26 17:32:20 -0800

    Solved R2.

---
+++ R2/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int R1 = scan.nextInt();
+        int S = scan.nextInt();
+        System.out.println((S*2)-R1);
+    }
+}
